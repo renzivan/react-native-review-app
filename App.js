979849b1309c42894f9b7de1f5d81b7a96dd95af
unknown <renzivanyapenguio@gmail.com>
2020-09-08 12:14:25 +0800

    added stack navigation

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import * as Font from 'expo-font'
 import Home from './screens/home'
 import { AppLoading } from 'expo'
+import Navigator from './routes/homeStack'
 
 const getFonts = () => {
   return Font.loadAsync({
@@ -15,7 +16,7 @@ export default function App() {
 
   if(fontsLoaded) {
     return (
-      <Home />
+      <Navigator />
     );
   } else {
     return (
